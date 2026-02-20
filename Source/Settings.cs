ec59ec9ea11a51f76a951a5644dc1abf08db8a33
jlibrary <35176781+jlibrary@users.noreply.github.com>
2026-02-20 00:01:19 -0500

    chore: v1.0.5

--- Source/Settings.cs
+++ Source/Settings.cs
@@ -8,7 +8,7 @@ namespace RimTalk;
 
 public partial class Settings : Mod
 {
-    public const string Version = "1.0.4";
+    public const string Version = "1.0.5";
 
     private Vector2 _mainScrollPosition = Vector2.zero;
     private Vector2 _aiInstructionScrollPos = Vector2.zero;
